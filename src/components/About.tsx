134da6e898c810c8d5e28902ace0b1e98cd0351d
Daniel Ojeda <danielojedamartinez43@gmail.com>
2025-07-27 11:35:13 -0600

    Estilos y separaciones

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 function About() {
   return (
-    <section className="p-4 text-center">
-      <h2>Perfil Profesional</h2>
-      <p>
+     <section className="container py-5 border-bottom">
+      <h2 className="text-center mb-4">Perfil Profesional</h2>
+      <p className="text-justify">
         Soy estudiante de Ingeniería en Sistemas con pasión por el desarrollo de software,
         experiencia en proyectos académicos y habilidades en tecnologías web y backend.
       </p>
--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,14 +1,14 @@
 function Education() {
   return (
-    <section className="p-4 bg-light text-dark text-center">
-      <h2>Formación Académica</h2>
+    <section className="container py-5 border-bottom">
+      <h2 className="text-center mb-4">Formación Académica</h2>
       <ul className="list-unstyled">
         <li>
           <strong>Universidad Mariano Gálvez de Guatemala</strong><br />
           Ingeniería en Sistemas<br />
           2025 - Actualidad
         </li>
-        <li className="mt-3">
+        <li className="mt-4">
           <strong>Instituto Nacional de Educación Diversificada</strong><br />
           Bachillerato en Ciencias y Letras con orientación en Computación<br />
           2020 - 2021
